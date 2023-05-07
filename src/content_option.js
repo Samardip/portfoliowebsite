4b0d29339e4ceec4884971e95f4907d2e19bdf7b
SamardipMandal <73586470+SamardipMandal@users.noreply.github.com>
2023-05-07 23:49:08 +0530

    added changes

--- src/content_option.js
+++ src/content_option.js
@@ -1,156 +1,179 @@
-const logotext = "JOHN";
+const logotext = "Samardip";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "Samardip Mandal",
+    description: "I am a Frontend Developer John Doe data scientist _ Full stack devloper,currently working in Berlin",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m Samardip Mandal",
     animated: {
-        first: "I love coding",
-        second: "I code cool websites",
-        third: "I develop mobile apps",
+        first: "1.2+ years of relevent experience",
+        second: "I am a Frontend Developer",
+        third: "HTML, CSS, Javascript, ReactJS",
     },
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
-    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+    description: "Immediate Joiner | I have 1.2+ years of relevent experience working as an Frontend developer using Html, CSS, Tailwind CSS,Javascript, Reactjs, Redux . I am open to learning and working on new technology and keep on improving.",
+    your_img_url: "/images/profileimg.jpg",
 };
 
 const dataabout = {
-    title: "abit about my self",
-    aboutme: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent quis dolor id ligula semper elementum feugiat pretium nulla. Nunc non commodo dolor. Nunc mollis dignissim facilisis. Morbi ut magna ultricies.",
+    title: "Fronten Developer",
+    aboutme: "I am a 2022 B. Tech graduate from RCC Institute of information Technology specialised in Computer Science Engineering. I have intermediate level skills of C++,JAVA, HTML, CSS,Tailwind CSS, javaScript, ReactJS, Redux. I have 7 months of work experience working as an Frontend Developer Intern and currently 8 months of relevent professional experience in ReactJS + Redux working for startup. I am a competitive coder with 4-star rating on CodeChef and 1154 max rating on codeforces.I am open to learning and working on new technologies along with gaining knowledge and experience.",
 };
-const worktimeline = [{
-        jobtitle: "Designer of week",
-        where: "YAdfi",
-        date: "2020",
-    },
-    {
-        jobtitle: "Designer of week",
-        where: "Jamalya",
-        date: "2019",
-    },
-    {
-        jobtitle: "Designer of week",
-        where: "ALquds",
-        date: "2019",
-    },
+const worktimeline = [
+    // {
+    //     jobtitle: "Designer of week",
+    //     where: "YAdfi",
+    //     date: "2020",
+    // },
+    // {
+    //     jobtitle: "Designer of week",
+    //     where: "Jamalya",
+    //     date: "2019",
+    // },
+    // {
+    //     jobtitle: "Designer of week",
+    //     where: "ALquds",
+    //     date: "2019",
+    // },
 ];
 
-const skills = [{
-        name: "Python",
+const skills = [
+    {
+        name: "C++",
         value: 90,
     },
     {
-        name: "Djano",
-        value: 85,
+        name: "HTML",
+        value: 80,
+    },
+    {
+        name: "CSS",
+        value: 80,
+    },
+    {
+        name: "Tailwind CSS",
+        value: 80,
+    },
+    {
+        name: "Bootstrap",
+        value: 80,
     },
     {
         name: "Javascript",
         value: 80,
     },
     {
-        name: "React",
+        name: "Typescript",
+        value: 50,
+    },
+    {
+        name: "ReactJS",
+        value: 80,
+    },
+    {
+        name: "Redux",
+        value: 80,
+    },
+    {
+        name: "React Native",
+        value: 50,
+    },
+    {
+        name: "Redux",
+        value: 80,
+    },
+    {
+        name: "NodeJS",
+        value: 30,
+    },
+    {
+        name: "MySQL",
         value: 60,
     },
     {
-        name: "Jquery",
-        value: 85,
+        name: "Redux",
+        value: 80,
+    },
+    {
+        name: "git",
+        value: 60,
+    },
+    {
+        name: "github",
+        value: 60,
+    },
+    {
+        name: "Rest API",
+        value: 70,
+    },
+    {
+        name: "AWS",
+        value: 20,
+    },
+    {
+        name: "DOM Object",
+        value: 70,
     },
 ];
 
-const services = [{
-        title: "UI & UX Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-    {
-        title: "Mobile Apps",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
-    {
-        title: "Wordpress Design",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
-    },
+const services = [
+    // {
+    //     title: "UI & UX Design",
+    //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    // },
+    // {
+    //     title: "Mobile Apps",
+    //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    // },
+    // {
+    //     title: "Wordpress Design",
+    //     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    // },
 ];
 
 const dataportfolio = [{
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/800/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-
-    {
-        img: "https://picsum.photos/400/600/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/300/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/550/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
-    {
-        img: "https://picsum.photos/400/700/?grayscale",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
-        link: "#",
-    },
+    img: "/images/proj1.png",
+    description: "chat and reply responsive frontend ui with delete and edit option and voting option, built in reactJS",
+    link: "https://data-poem-assignment.vercel.app/",
+},
+{
+    img: "/images/proj2.png",
+    description: "Responsive To-dos List application with edit and delete option in ReactJS",
+    link: "https://ineuronassesment.vercel.app/",
+},
+{
+    img: "/images/proj3.png",
+    description: "A cross chain crypto wallet application build from scratch where you can mint and sale nfts and hold utility based coins - NFTVerse Team Project",
+    link: "https://www.talewallet.com",
+},
+{
+    img: "/images/proj4.png",
+    description: "Crypto wallet android app build from scratch using react native and typescript - NFTVerse Team Project",
+    link: "https://play.google.com/store/apps/details?id=com.talewallet",
+},
+{
+    img: "/images/proj5.png",
+    description: "Demo frontend Hotel Management UI",
+    link: "https://Samardip.github.io/Demo_UI",
+},
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    YOUR_EMAIL: "samardipmandal.kgp@gmail.com",
+    YOUR_FONE: "+91 7364923708",
+    description: " ",
     // creat an emailjs.com account 
     // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
-    YOUR_SERVICE_ID: "service_id",
-    YOUR_TEMPLATE_ID: "template_id",
-    YOUR_USER_ID: "user_id",
+    // YOUR_SERVICE_ID: "service_id",
+    // YOUR_TEMPLATE_ID: "template_id",
+    // YOUR_USER_ID: "user_id",
 };
 
 const socialprofils = {
-    github: "https://github.com",
-    facebook: "https://facebook.com",
-    linkedin: "https://linkedin.com",
-    twitter: "https://twitter.com",
+    github: "https://github.com/Samardip",
+    facebook: "https://www.facebook.com/samardip.mandal/",
+    linkedin: "https://www.linkedin.com/in/samardipmandal/",
+    // twitter: "https://twitter.com",
 };
 export {
     meta,
